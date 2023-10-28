0afaeb8d554d4e7178a8a537d7076c806b8b9893
Ahmed Ali <ahmedmuhammad.aliamin@gmail.com>
2023-10-28 14:36:09 +0200

    init our iOS project

---
+++ GitExploreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitExploreApp
+//
+//  Created by Ahmed Ali on 28/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
