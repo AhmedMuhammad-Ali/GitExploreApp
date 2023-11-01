3b5da23cb40cd6ada6c8a200b231b29f6bf5e3f3
Ahmed Ali <ahmedmuhammad.aliamin@gmail.com>
2023-11-01 20:17:21 +0200

    add constants file

---
+++ GitExploreApp/System /Dimensions.swift
@@ -0,0 +1,13 @@
+//
+//  Dimensions.swift
+//  GitExploreApp
+//
+//  Created by Ahmed Ali on 30/10/2023.
+//
+
+enum Dimensions {
+    static let d2 = 2.0
+    static let d12 = 12.0
+    static let d24 = 24.0
+    static let d70 = 70.0
+}
