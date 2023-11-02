92abca002c212caf882b6d1a67f285b86cc0a16f
Ahmed Ali <ahmedmuhammad.aliamin@gmail.com>
2023-11-02 22:16:46 +0200

    open the app using the app coordinator

--- GitExploreApp/SceneDelegate.swift
+++ GitExploreApp/SceneDelegate.swift
@@ -5,16 +5,24 @@
 //  Created by Ahmed Ali on 28/10/2023.
 //
 
-import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var appCoordinator: Coordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        let appCoordinator = AppCoordinator(window: window,
+                                            navigationController: UINavigationController())
+        self.appCoordinator = appCoordinator
+        appCoordinator.start()
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
